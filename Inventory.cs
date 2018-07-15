76544d9f4c157b08c698407330ade9670e11b628
kevlinkevlin <36911366+kevlinkevlin@users.noreply.github.com>
2018-07-15 19:46:23 +0800

    PassWordGame

---
+++ Inventory.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Fungus;
+public class Inventory : MonoBehaviour, IHasChanged
+{
+    [SerializeField]
+    Transform slots;
+    [SerializeField]
+    Text inventoryText;
+    [SerializeField]
+    Flowchart flowchart;
+    [SerializeField]
+    AudioSource putAudioSource;
+    protected AudioSource openAudioSource;
+    
+    // Use this for initialization
+    void Awake()
+    {
+        openAudioSource = GetComponent<AudioSource>();
+    }
+    void Start()
+    {
+        
+        HasChanged();
+    }
+
+    #region IHasChanged implementation
+    public void HasChanged()
+    {
+        
+        System.Text.StringBuilder builder = new System.Text.StringBuilder();
+        //builder.Append(" - ");
+        foreach (Transform slotTransform in slots)
+        {
+           
+            GameObject item = slotTransform.GetComponent<Slot>().item;
+            if (item)
+            {   
+                PutSound();
+                builder.Append(item.name);
+                //   builder.Append(" - ");
+            }
+        }
+        if (builder.ToString() == "LIGHT" && flowchart.GetBooleanVariable("磚塊判斷") == false)
+        {
+            inventoryText.text = "complete";
+            flowchart.SetBooleanVariable("磚塊判斷", true);
+            OpenSound();
+            Flowchart.BroadcastFungusMessage("磚塊");
+        }
+        else
+        {
+            inventoryText.text = builder.ToString();
+        }
+        if (builder.ToString() == "216435"&& flowchart.GetBooleanVariable("排序判斷")==false)
+        {
+            inventoryText.text = "complete";
+            flowchart.SetBooleanVariable("排序判斷", true);
+            OpenSound();
+            Flowchart.BroadcastFungusMessage("排序");
+        }
+        else 
+        {
+            inventoryText.text = builder.ToString();
+        }
+    }
+    #endregion
+    protected void OpenSound()
+    {
+        if (openAudioSource != null)
+        {
+            openAudioSource.Play();
+        }
+    }
+    protected void PutSound()
+    {
+        if (putAudioSource != null)
+        {
+            putAudioSource.Play();
+        }
+    }
+}
+
+
+namespace UnityEngine.EventSystems
+{
+    public interface IHasChanged : IEventSystemHandler
+    {
+        void HasChanged();
+    }
+}
---
+++ Orb.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+public class Orb : MonoBehaviour
+{
+    public enum OrbsType { Fire, Ice, Wood, Shine, Dark, Heart, Null };
+    public OrbsType type;
+    private Sprite[] typeImage;
+    public Image image;
+    public RectTransform imageRect;
+
+    public float width;
+    public float height;
+    
+    public int row;
+    public int column;
+    public int number;
+    public List<Orb> linkOrbs = new List<Orb>();
+    public bool group = false;
+    public bool removed = false;
+    
+    public enum OrbsState { Create, Change, Remove, Stay };
+    public OrbsState state;
+    public float removeTime;
+    private float timer = 0;
+    public float removeAlpha;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Finger")
+        {
+            image.color = new Color(1, 1, 1, 0);
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Finger" && other.GetComponent<Finger>().moveState == false)
+        {
+            image.color = Color.white;
+        }
+    }
+    public void RemoveAni()
+    {
+        timer += Time.deltaTime;
+        if (timer > removeTime)
+        {
+            if (image.color.a > 0)
+            {
+                image.color = new Color(1, 1, 1, image.color.a - removeAlpha * Time.deltaTime);
+            }
+            else
+            {
+                state = OrbsState.Stay;
+                timer = 0;
+            }
+        }
+        
+    }
+    public void SetAniPos(Vector2 dir, int count, OrbsState state)
+    {
+        imageRect.anchoredPosition = new Vector2(width * dir.x, height * dir.y) * count;
+        this.state = state;
+    }
+    public void MoveAni()
+    {
+        image.color = Color.white;
+        imageRect.anchoredPosition = Vector2.MoveTowards(imageRect.anchoredPosition, Vector2.zero, 2000 * Time.deltaTime);
+        if (imageRect.anchoredPosition == Vector2.zero)
+            state = OrbsState.Stay;
+    }
+    void Start()
+    {
+        transform.name = "orb" + row + column;
+        GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+        typeImage = new Sprite[(int)OrbsType.Null];
+        for (int i = 0; i < typeImage.Length; i++)
+        {
+            typeImage[i] = Resources.Load<Sprite>("Image/" + (OrbsType)i);
+        }
+
+        GetComponent<BoxCollider2D>().offset = new Vector2(width / 2, height / 2);
+        GetComponent<BoxCollider2D>().size = new Vector2(width, height);
+
+        imageRect.GetComponent<RectTransform>().sizeDelta = new Vector2(width, height);
+        state = OrbsState.Create;
+    }
+    void Update()
+    {
+        image.sprite = typeImage[(int)type];
+        switch (state)
+        {
+            case OrbsState.Create:
+            case OrbsState.Change:
+                MoveAni();
+                break;
+            case OrbsState.Remove:
+                RemoveAni();
+                break;
+        }
+    }
+}
